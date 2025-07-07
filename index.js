38beb72c8a7014f799fdc41be6da64cdd4ba92ee
Mathew de Vin <mathewdevin03@gmail.com>
2025-07-07 13:05:32 -0700

    Fix module type conflicts

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-const eslintConfig = require("./eslint.config")
-module.exports = eslintConfig
+const pluginVue = require("eslint-plugin-vue");
+const stylistic = require("@stylistic/eslint-plugin");
+
+module.exports = [
+	...pluginVue.configs['flat/strongly-recommended'],
+	{
+		rules: {
+			'@stylistic/indent': ['error', 'tab'],
+			'vue/html-indent': ['error', 'tab'],
+			'@stylistic/eol-last': ['error', 'always'],
+			'@stylistic/comma-dangle': ['error', 'always-multiline'],
+			'@stylistic/object-curly-spacing': ['error', 'always'],
+			'@stylistic/semi': ['error', 'never'],
+		},
+		plugins: {
+			'@stylistic': stylistic,
+		},
+		languageOptions: {
+			sourceType: 'module',
+		},
+	},
+]
