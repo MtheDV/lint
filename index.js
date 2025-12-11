1baf8589849d470cb38736f626c64a2288503572
Mathew de Vin <mathewdevin03@gmail.com>
2025-12-11 12:04:59 -0800

    Update eslint to use React instead of Vue

--- index.js
+++ index.js
@@ -1,35 +1,40 @@
-import pluginVue from "eslint-plugin-vue"
 import stylistic from '@stylistic/eslint-plugin'
 import tseslint from "typescript-eslint"
-import vueParser from 'vue-eslint-parser'
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
 
 export default [
-	// Default TS and VUE eslint configs
-	...tseslint.configs.recommended,
-	...pluginVue.configs['flat/strongly-recommended'],
 	{
-		// For TS, TSX, and VUE files
-		files: ['**/*.ts', '**/*.tsx', '**/*.vue'],
-		languageOptions: {
-			parser: vueParser,
-			parserOptions: {
-				parser: tseslint.parser,
-				sourceType: 'module',
-				ecmaVersion: 'latest',
-			},
-		},
+		files: ['**/*.{ts,tsx}'],
+		extends: [
+			js.configs.recommended,
+			tseslint.configs.recommendedTypeChecked,
+			reactHooks.configs.flat.recommended,
+			reactRefresh.configs.vite,
+		],
 		plugins: {
 			'@stylistic': stylistic,
 			'@typescript-eslint': tseslint.plugin,
 		},
+		languageOptions: {
+			globals: globals.browser,
+			parser: tseslint.parser,
+			parserOptions: {
+				sourceType: 'module',
+				ecmaVersion: 'latest',
+				project: './tsconfig.json',
+			},
+		},
 		rules: {
 			// Styling rules
 			'@stylistic/indent': ['error', 'tab'],
-			'vue/html-indent': ['error', 'tab'],
 			'@stylistic/eol-last': ['error', 'always'],
 			'@stylistic/comma-dangle': ['error', 'always-multiline'],
 			'@stylistic/object-curly-spacing': ['error', 'always'],
 			'@stylistic/semi': ['error', 'never'],
+			'@stylistic/quotes': ['error', 'single'],
 
 			// TS specific rules
 			'@typescript-eslint/no-explicit-any': 'warn',
@@ -39,17 +44,6 @@ export default [
 				argsIgnorePattern: '^_',
 				varsIgnorePattern: '^_',
 			}],
-		},
-	},
-	{
-		// Additional overrides for pure TS/TSX files
-		files: ['**/*.ts', '**/*.tsx'],
-		languageOptions: {
-			parser: tseslint.parser,
-			parserOptions: {
-				sourceType: 'module',
-				ecmaVersion: 'latest',
-			},
 		},
 	},
 ]
